35e53d8383653230a67b3700412a9b918be407c6
Darwin Algarin <dalgarins@live.com>
2020-02-02 11:52:39 -0500

    added beans for claro login

--- src/main/java/co/anbora/labs/claro/injection/DataModule.java
+++ src/main/java/co/anbora/labs/claro/injection/DataModule.java
@@ -2,25 +2,26 @@ package co.anbora.labs.claro.injection;
 
 import co.anbora.labs.claro.data.remote.ClaroVideoRepositoryImpl;
 import co.anbora.labs.claro.data.remote.api.rest.ClaroMFWKWebApi;
-import co.anbora.labs.claro.data.remote.model.login.LoginDTO;
-import co.anbora.labs.claro.domain.model.claro.LoginToken;
+import co.anbora.labs.claro.data.remote.mapper.LoginDTOMapper;
+import co.anbora.labs.claro.domain.model.claro.Credential;
 import co.anbora.labs.claro.domain.repository.IClaroVideoRepository;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
 
-import java.util.function.Function;
+import javax.inject.Singleton;
 
 @Factory
 public class DataModule {
 
     @Bean
-    IClaroVideoRepository provideRemoteRepository(ClaroMFWKWebApi loginApi) {
-        return new ClaroVideoRepositoryImpl(new Function<LoginDTO, LoginToken>() {
-            @Override
-            public LoginToken apply(LoginDTO loginDTO) {
-                return null;
-            }
-        }, loginApi);
+    @Singleton
+    LoginDTOMapper provideLoginToTokenMapper() {
+        return new LoginDTOMapper();
+    }
+
+    @Bean
+    IClaroVideoRepository provideRemoteRepository(Credential credential, LoginDTOMapper loginDTOMapper, ClaroMFWKWebApi loginApi) {
+        return new ClaroVideoRepositoryImpl(credential, loginDTOMapper, loginApi);
     }
 
 
--- src/main/java/co/anbora/labs/claro/injection/DomainModule.java
+++ src/main/java/co/anbora/labs/claro/injection/DomainModule.java
@@ -1,14 +1,23 @@
 package co.anbora.labs.claro.injection;
 
+import co.anbora.labs.claro.domain.model.claro.Credential;
 import co.anbora.labs.claro.domain.repository.IClaroVideoRepository;
 import co.anbora.labs.claro.domain.usecase.login.GetCookiesUseCase;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Property;
 
 import javax.inject.Singleton;
 
 @Factory
 public class DomainModule {
+
+    @Bean
+    @Singleton
+    Credential provideClaroCredential(@Property(name = "claro.user") String username,
+                                      @Property(name = "claro.password") String password) {
+        return new Credential(username, password);
+    }
 
     @Bean
     @Singleton
