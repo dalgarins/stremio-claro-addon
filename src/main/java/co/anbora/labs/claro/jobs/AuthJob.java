90a37c83a1585f4ac5c49956526a39deb422120f
Darwin Algarin <dalgarins@live.com>
2020-02-02 22:06:13 -0500

    added job to auth every day

--- src/main/java/co/anbora/labs/claro/jobs/AuthJob.java
+++ src/main/java/co/anbora/labs/claro/jobs/AuthJob.java
@@ -1,11 +1,26 @@
 package co.anbora.labs.claro.jobs;
 
-import javax.inject.Singleton;
+import co.anbora.labs.claro.domain.usecase.UseCaseExecutor;
+import co.anbora.labs.claro.domain.usecase.login.GetCookiesUseCase;
 import io.micronaut.scheduling.annotation.Scheduled;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.function.Function;
 
 @Singleton
 public class AuthJob {
 
-    @Scheduled(fixedRate = "5m")
-    public void process() {}
+    @Inject
+    UseCaseExecutor useCaseExecutor;
+    @Inject
+    GetCookiesUseCase getCookiesUseCase;
+
+    @Scheduled(fixedRate = "1440m")
+    public void process() {
+        useCaseExecutor.execute(getCookiesUseCase,
+                new GetCookiesUseCase.Request(),
+                Function.identity()
+        );
+    }
 }
