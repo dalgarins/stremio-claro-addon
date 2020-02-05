13d44b6e28f022375f0893fd5071e1eb9d54bd40
Darwin Algarin <dalgarins@live.com>
2020-02-04 22:56:57 -0500

    syncronized getcookies and getcategories use case

--- src/main/java/co/anbora/labs/claro/jobs/AuthJob.java
+++ src/main/java/co/anbora/labs/claro/jobs/AuthJob.java
@@ -1,11 +1,14 @@
 package co.anbora.labs.claro.jobs;
 
 import co.anbora.labs.claro.domain.usecase.UseCaseExecutor;
+import co.anbora.labs.claro.domain.usecase.category.GetCategoriesUseCase;
 import co.anbora.labs.claro.domain.usecase.login.GetCookiesUseCase;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.scheduling.annotation.Scheduled;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 @Singleton
@@ -15,12 +18,27 @@ public class AuthJob {
     UseCaseExecutor useCaseExecutor;
     @Inject
     GetCookiesUseCase getCookiesUseCase;
+    @Inject
+    GetCategoriesUseCase getCategoriesUseCase;
+    @Property(name = "claro.node_movies")
+    String nodeMovies;
 
     @Scheduled(fixedRate = "1440m")
     public void process() {
-        useCaseExecutor.execute(getCookiesUseCase,
+        login().thenApply(this::getCategories);
+    }
+
+    private CompletableFuture<GetCookiesUseCase.Response> login() {
+        return useCaseExecutor.execute(getCookiesUseCase,
                 new GetCookiesUseCase.Request(),
                 Function.identity()
         );
+    }
+
+    private CompletableFuture<GetCategoriesUseCase.Response> getCategories(
+            GetCookiesUseCase.Response cookies) {
+        return useCaseExecutor.execute(getCategoriesUseCase,
+                new GetCategoriesUseCase.Request(nodeMovies),
+                Function.identity());
     }
 }
