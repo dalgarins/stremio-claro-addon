fceb33c95052014df8b32359e41d181c8728304e
Darwin Algarin <dalgarins@live.com>
2020-02-09 22:56:18 -0500

    added stremio constants

---
+++ domain/src/main/java/co/anbora/labs/claro/domain/constants/StremioConstants.java
@@ -0,0 +1,24 @@
+package co.anbora.labs.claro.domain.constants;
+
+import co.anbora.labs.claro.domain.model.stremio.Catalog;
+
+public class StremioConstants {
+
+    private StremioConstants() {}
+
+    public interface Plugin {
+        String ID = "co.anbora.labs.claro.videos";
+        String VERSION = "1.0";
+        String NAME = "Claro Videos";
+        String DESCRIPTION = "Plugin for Claro videos Colombia";
+        String[] ID_PREFIXES = null;
+        String[] RESOURCES = new String[] {"catalog", "stream"};
+        String[] TYPES = new String[] {"movie"};
+        Catalog[] CATALOGS = new Catalog[] {new Catalog("movie", "claroCatalog", "Claro Movies Catalog", new String[] {}, new String[0], new String[] {"search"})};
+        String BACKGROUND = "";
+        String LOGO = "https://upload.wikimedia.org/wikipedia/commons/0/0e/LogoClaroVid.png";
+        String CONTACT_EMAIL = "anboralabs@gmail.com";
+
+    }
+
+}
